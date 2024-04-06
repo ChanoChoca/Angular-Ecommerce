38312c9e0f3f6311cafb6f460543106e452de6ec
Juan Ignacio Caprioli <juanicaprioli16@outlook.com>
2024-04-06 13:00:03 -0300

    Environment configuration: test with npm start -- --configuration=qa

---
+++ README.md
@@ -0,0 +1,9 @@
+¡Hola! Bienvenido a mi proyecto Ecommerce.
+
+En el proyecto se realizan compras de cursos y otras cosas.
+
+Que estoy utilizando:
+- Angular Framework
+- Spring Boot Framework
+- Bootstrap
+- Git
---
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oag6caahqSSXNq1x5d7',
+        issuer: 'https://dev-36208380.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
